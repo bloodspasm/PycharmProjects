b64911a6bd18242a442211661f6cfd8cdce440b7
fox <chlliao@yaduo.com>
2017-03-01 18:47:59 +0800

    Demo

--- Demo/tensorflow12_plut_result.py
+++ Demo/tensorflow12_plut_result.py
@@ -1,4 +1,3 @@
-# -*-coding:gbk-*-
 # View more python learning tutorial on my Youtube and Youku channel!!!
 
 # Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
@@ -10,6 +9,7 @@ Please note, this code is only for python 3+. If you are using python 2+, please
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
@@ -22,57 +22,60 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
         outputs = activation_function(Wx_plus_b)
     return outputs
 
-
 # Make up some real data
-
-# 间隔采样 numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None)[source] 维度
-# 1 个神经元
-x_data = np.linspace(-1, 1, 300)[:, np.newaxis]  # 矩阵变成1 列的
-print (x_data)
-print ("####################")
-
-# 此概率分布的均值（对应着整个分布的中心centre）
-# 此概率分布的标准差（对应于分布的宽度，scale越大越矮胖，scale越小，越瘦高）
-# 输出的shape(格式)，默认为None，只输出一个值
-noise=np.random.normal(0, 0.05, x_data.shape)
-print (noise)
-print ("####################")
-y_data=np.square(x_data) - 0.5 + noise
+x_data = np.linspace(-1,1,300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
 
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 # add hidden layer
-# 隐藏层
-# add_layer(输入, 输入神经元, 输出神经元, 激励函数)
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 # add output layer
-# 输出层
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-# the error between prediction and real data
-# 误差 reduce_mean 平均值   reduce_sum求和 tf.square 平方
-# reduction_indices的默认值时None，即把input_tensor降到 0维，也就是一个数。
-# 对于2维input_tensor，reduction_indices=0时，按列；reduction_indices=1时，按行。
+# the error between prediciton and real data
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                                    reduction_indices=[1]))
-
-# 练习  0.1=学习效率 minimize=减下误差
+                     reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # important step
 # tf.initialize_all_variables() no long valid from
 # 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12:
+if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
     init = tf.initialize_all_variables()
 else:
     init = tf.global_variables_initializer()
-sess = tf.Session() # 初始化服务
-sess.run(init)  # 初始化变量
+sess = tf.Session()
+sess.run(init)
 
-for i in range(10000):
+# plot the real data
+# 设置图的大小，添加子图
+# fig = plt.figure(figsize=(5,5))
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+#绘制sin, cos
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
+for i in range(1000):
     # training
+    # 练习
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
-        # to see the step improvement
-        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        # to visualize the result and improvement
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        # 获取prediction_value
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        # plot the prediction
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(0.1)
+
+
+
+
