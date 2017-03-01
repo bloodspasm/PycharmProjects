1f8a4f76a987d2b527d89c9605e9423d6fc94337
fox <chlliao@yaduo.com>
2017-03-01 14:05:59 +0800

    Demo

--- Demo/TensorflowDemo.py
+++ Demo/TensorflowDemo.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 import numpy as np
 
 # 使用 NumPy 生成假数据(phony data), 总共 100 个点.
-x_data = np.float32(np.random.rand(2, 100)) # 随机输入
+x_data = np.float32(np.random.rand(2, 100) )  # 随机输入 2 行 100 列
 y_data = np.dot([0.100, 0.200], x_data) + 0.300
+
 
 # 构造一个线性模型
 #
@@ -30,4 +31,4 @@ for step in xrange(0, 201):
     if step % 20 == 0:
         print step, sess.run(W), sess.run(b)
 
-# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
+        # 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
