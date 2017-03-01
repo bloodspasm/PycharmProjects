7b9f289dcf659b928782a1b6ed16789d1fc50cee
fox <chlliao@yaduo.com>
2017-03-01 18:48:28 +0800

    Demo

--- Demo/tensorflow12_plut_result.py
+++ Demo/tensorflow12_plut_result.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
