672fca4d4d93e3c84d085c8f45e0bb9f30b1ae3b
fox <chlliao@yaduo.com>
2017-03-03 11:33:00 +0800

    Demo

---
+++ Demo/tf15_tensorboard/tf15.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/3 AM11:17
+# @Author  : bloodspasm
+# @Site    : 
+# @File    : tf15.py
+# @Software: PyCharm
+
+
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+
+print (tf.__version__)
